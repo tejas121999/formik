3ad59dbcc37d9d3fcdbda180297e4d1828a529d0
tejasMetricoidtech <tejas@metricoidtech.com>
2021-10-05 21:16:15 +0530

    commit

--- src/components/YtForm.js
+++ src/components/YtForm.js
@@ -1,90 +1,71 @@
 import React from 'react'
-import { useFormik } from 'formik'
+import { ErrorMessage, Field, Form, Formik } from 'formik'
 import * as Yup from 'yup'
 
+
+
+const initialValues = {
+    name: '',
+    email: '',
+    channal: ''
+}
+
+const onSubmit = values => {
+    console.log('Form values', values)
+}
+
+
+const validationSchema = Yup.object({
+    name: Yup.string().required('Required'),
+    email: Yup.string().email('Invilade Email Format').required('Required'),
+    channal: Yup.string().required('Required')
+})
+
+
+
+// console.log('visited field', formik.touched)
 function YtForm() {
-
-    const initialValues = {
-        name: '',
-        email: '',
-        channal: ''
-    }
-
-    const onSubmit = values => {
-        console.log('Form values', values)
-    }
-
-
-    const validationSchema = Yup.object({
-        name: Yup.string().required('Required'),
-        email: Yup.string().email('Invilade Email Format').required('Required'),
-        channal: Yup.string().required('Required')
-    })
-
-    const formik = useFormik({
-        initialValues,
-        onSubmit,
-        validationSchema
-    })
-
-    // console.log('visited field', formik.touched)
-
+   
     return (
-        <div>
-            <form onSubmit={formik.handleSubmit}>
+        <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}
+        >
+            <Form>
                 <div className='form-control'>
                     <label htmlFor='name'>Name</label>
-                    <input
+                    <Field
                         type='text'
                         id='name'
                         name='name'
-                        onChange={formik.handleChange}
-                        value={formik.values.name}
-                        onBlur={formik.handleBlur}
                     />
-                    {
-                        formik.touched.name && formik.errors.name ?
-                            <div className='error'>{formik.errors.name}</div>
-                            : null
-                    }
+                    <ErrorMessage name='name' />
                 </div>
 
                 <div className='form-control'>
                     <label htmlFor='email'>E-mail</label>
-                    <input
+                    <Field
                         type='email'
                         id='email'
                         name='email'
-                        onChange={formik.handleChange}
-                        value={formik.values.email}
-                        onBlur={formik.handleBlur}
+                      
                     />
-                    {
-                        formik.touched.email && formik.errors.email ?
-                            <div className='error'>{formik.errors.email}</div>
-                            : null
-                    }
+                    <ErrorMessage name='email' />
                 </div>
 
                 <div className='form-control'>
                     <label htmlFor='channal'>Channal</label>
-                    <input
+                    <Field
                         type='text'
                         id='channal'
                         name='channal'
-                        onChange={formik.handleChange}
-                        value={formik.values.channal}
-                        onBlur={formik.handleBlur}
                     />
-                    {
-                        formik.touched.channal && formik.errors.channal ?
-                            <div className='error'>{formik.errors.channal}</div>
-                            : null
-                    }
+                    <ErrorMessage name='channal' />
                 </div>
                 <button type="submit">Submit</button>
-            </form>
-        </div>
+            </Form>
+        </Formik>
     )
 }
 
